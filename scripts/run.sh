f6ef72234f39297f509e7fe4749f8c86428957db
Gilberto Marcon <gilbertoamarcon@gmail.com>
2017-12-04 14:47:50 -0800

    New Run script

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,9 +1,18 @@
 #!/bin/bash
 
+solver=src/solvers/GMAA-ICE-CUSTOM-HEUR
+# solver=src/solvers/DICEPS
+
 # Running
-src/solvers/GMAA-ICE -h5 DT
+for h in {2..10}; do
+	echo "Horizon $h..."
+	# $solver -q -h $h ${HOME}/.madp/problems/recycling.dpomdp
+	# $solver -q -h $h FFG
+	$solver -q -h $h ${HOME}/.madp/problems/FFG343.pgmx
+done;
 
 # Plotting policies
+echo "Policy Plot..."
 pol_list=$(find ${HOME}/.madp/results/ -type f -name *_JPol)
 for p in $pol_list; do
 	python scripts/policy-plot.py $p
